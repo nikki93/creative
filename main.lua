6be4a1f5ec1c83ee5ef2a3b0eb7f99599a778b3c
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-06-21 14:09:30 -0700

    start work on 'mod' concept

--- main.lua
+++ main.lua
@@ -1,79 +1,32 @@
--- Global libary aliases
-
 L = require('https://raw.githubusercontent.com/nikki93/L/3f63e72eef6b19a9bab9a937e17e527ae4e22230/L.lua')
 
 
-local code = [[
+local Mod = require 'mod'
+
+
+local circle = Mod.new({
+    name = 'circle',
+    code = [[
 local radius = 40
 
-function panel()
+function E.ui()
     radius = L.ui.slider('radius', radius, 20, 100)
 end
 
-function draw()
-    L.circle('fill', 400, 400, radius)
+function E.draw()
+    L.circle('fill', 200, 300, radius)
 end
-]]
-
-local lastChangeTime
-
-local namespace
-
-local function reset()
-    namespace = setmetatable({}, { __index = _G })
-end
-
-reset()
-
-local function compile()
-    local compiled, err = load(code, 'code', 't', namespace)
-    if compiled then
-        compiled()
-    else
-        reset()
-        error(err)
-    end
-end
-
-compile()
-
-local function safeCall(foo)
-    if foo then
-        local succ, err = pcall(foo)
-        if not succ then
-            reset()
-            error(err)
-        end
-    end
-end
+    ]],
+})
 
 
 function castle.uiupdate()
-    L.ui.section('code', { defaultOpen = true }, function()
-        local newCode = L.ui.codeEditor('code', code, {
-            hideLabel = true,
-        })
-        if newCode ~= code then
-            code = newCode
-            lastChangeTime = L.getTime()
-        end
-    end)
-
-    L.ui.section('panel', { defaultOpen = true }, function()
-        safeCall(namespace.panel)
-    end)
-end
-
-function love.update()
-    if lastChangeTime ~= nil and L.getTime() - lastChangeTime > 0.4 then
-        lastChangeTime = nil
-        compile()
-    end
+    circle:ui()
 end
 
 function love.draw()
     L.stacked('all', function()
-        safeCall(namespace.draw)
+        circle:safeCall('draw')
     end)
 
     L.print('fps: ' .. L.getFPS(), 20, 20)
---
+++ mod.lua
@@ -0,0 +1,89 @@
+local Mod = {}
+
+
+local nextId = 1
+
+
+function Mod:new()
+    self = self or {}
+    setmetatable(self, { __index = Mod })
+
+    self.id = nextId
+    nextId = nextId + 1
+
+    self.name = self.name or 'mod' .. self.id
+    self.code = self.code or ''
+
+    self.lastCodeChangeTime = nil
+    self.errored = false
+
+    self.E = {}
+
+    self:compile()
+
+    return self
+end
+
+
+function Mod:compile()
+    self.lastCodeChangeTime = nil
+    self.errored = false
+
+    local env = setmetatable({ E = self.E }, { __index = _G })
+    local compiled, err = load(self.code, 'code', 't', env)
+
+    if compiled then
+        self:safeCall(compiled)
+    else
+        self:error(err)
+    end
+end
+
+function Mod:error(err)
+    self.errored = true
+    error(err, 0)
+end
+
+
+function Mod:safeCall(nameOrFunc, ...)
+    if self.errored then
+        return
+    end
+
+    local func
+    if type(nameOrFunc) == 'string' then
+        func = self.E[nameOrFunc]
+    else
+        func = nameOrFunc
+    end
+
+    if func then
+        local succeeded, err = pcall(func, ...)
+        if not succeeded then
+            self:error(err)
+        end
+    end
+end
+
+
+function Mod:ui(props)
+    props = props or {}
+
+    L.ui.section(self.name, { defaultOpen = props.defaultOpen }, function()
+        L.ui.codeEditor('code', self.code, {
+            hideLabel = true,
+            onChange = function()
+                self.lastCodeChangeTime = L.getTime()
+            end,
+        })
+
+        self:safeCall('ui')
+    end)
+
+    if self.lastCodeChangeTime ~= nil and L.getTime() - self.lastCodeChangeTime >= 0.4 then
+        self:compile()
+    end
+end
+
+
+return Mod
