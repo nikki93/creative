cd1bb475c07dd8efadd8eb01105fc854bd01c9f4
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-06-21 16:50:39 -0700

    stuff

--- main.lua
+++ main.lua
@@ -7,25 +7,31 @@ local Mod = require 'mod'
 local mods = {}
 
 
-mods['main'] = Mod.new({
-    name = 'main',
-    code = [[
-radius = restored.radius or 40
-
-function draw()
-    L.circle('fill', 200, 300, radius)
+function refer(modName)
+    local foundMod = mods[modName]
+    if foundMod then
+        return foundMod.env
+    end
+    return nil
 end
 
-function panel()
-    radius = L.ui.slider('radius', radius, 20, 100)
+function add(mod)
+    mods[mod.name] = mod
 end
-]],
-})
+
+function remove(mod)
+    mods[mod.name] = nil
+end
 
 
-local selectedMod = nil
+local selectedModName = nil
 
 function castle.uiupdate()
+    local selectedMod = mods[selectedModName]
+    if not selectedMod then
+        selectedModName = nil
+    end
+
     -- Dropdown box for selecting a mod
     do
         local modNames = {}
@@ -35,10 +41,9 @@ function castle.uiupdate()
         table.sort(modNames, function(a, b)
             return a:upper() < b:upper()
         end)
-        local selectedModName = L.ui.dropdown('Selected mod name', selectedMod and selectedMod.name, modNames, {
+        selectedModName = L.ui.dropdown('Selected mod name', selectedModName, modNames, {
             hideLabel = true,
         })
-        selectedMod = mods[selectedModName]
     end
 
     L.ui.markdown('----')
@@ -51,10 +56,13 @@ function castle.uiupdate()
             mods[selectedMod.name] = nil
             selectedMod.name = newName
             mods[newName] = selectedMod
+            selectedModName = newName
         end
 
+        -- Code editor
         selectedMod:codeEditor('code')
 
+        -- Panel
         selectedMod:safeCall('panel')
     end
 end
@@ -69,3 +77,30 @@ function love.draw()
 
     L.print('fps: ' .. L.getFPS(), 20, 20)
 end
+
+
+add(Mod.new({
+    name = 'circle',
+    code = [[
+radius = restored.radius or 40
+
+function draw()
+    L.circle('fill', 200, 300, radius)
+end
+
+function panel()
+    radius = L.ui.slider('radius', radius, 20, 100)
+end
+]],
+}))
+
+add(Mod.new({
+    name = 'main',
+    code = [[
+circle = refer 'circle'
+
+function draw()
+    circle.draw()
+end
+]],
+}))
